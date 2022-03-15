485212aa5480a8fc6d3b1496e39e1940f5e6f0c9
Warwick Stone <warwick_stone@fastmail.com>
2022-03-15 04:33:59 -0700

    init with tf POC

---
+++ beginner_train_mnist.py
@@ -0,0 +1,4 @@
+""" From https://www.tensorflow.org/tutorials/quickstart/beginner
+"""
+import tensorflow as tf
+print("TensorFlow version:", tf.__version__)
---
+++ readme.md
@@ -0,0 +1,8 @@
+# Tensorflow cheatsheets
+
+python 3.9
+
+```sh
+source .venv/bin/activate
+pip install -r requirements.txt
+```
